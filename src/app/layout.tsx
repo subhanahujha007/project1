19a913e631fa9313c35713555b5f0abb691bafcf
subhanshu jha <subhanshujha001@gmail.com>
2024-02-26 14:02:28 +0530

    debug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <StarsCanvas/>
         <Navbar/>
         {children}
-        <Footer/>
+        
         </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Hero from "@/app/Hero/page"
 import Skills from "./skills/page";
+import Footer from "./Footer/page";
 export default function Home() {
   return (
     <main className="h-full w-full">
      <div className="flex flex-col h-[850px] gap-20">
       <Hero/>
-     
+      <Footer/>
      </div>
     </main>
   );
