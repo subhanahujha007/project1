a3f106b911662786ec91dcb63541ee4ccaed7f9f
subhanshu jha <subhanshujha001@gmail.com>
2024-02-26 13:46:21 +0530

    changes

--- src/app/Footer/page.tsx
+++ src/app/Footer/page.tsx
@@ -1,78 +1,73 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import {FaInstagramSquare} from "react-icons/fa"
 import Link from "next/link";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-  RxLinkedinLogo,
-} from "react-icons/rx";
-
-import { FaYoutube } from "react-icons/fa";
-
+const FooterSocialLinks = [
+  {
+    name: "Twitter",
+    href: "https://twitter.com/035Subhans67914",
+    icon: <FaTwitter />,
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/subhanshu-jha-1429a1195/",
+    icon: <FaLinkedin />,
+  },
+  {
+    name: "Github",
+    href: "https://github.com/subhanahujha007",
+    icon: <FaGithub />,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/jha.subhanshu/",
+    icon: <FaInstagramSquare />,
+  },
+];
+const FooterAboutLinks = [
+  {
+    name: "Learn about me",
+    href: "/subhanshujha.pdf",
+  },
+  {
+    name: "subhanshujha001@gmail.com",
+    href: "mailto:subhanshujha001@gmail.com",
+  },
+];
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-        <div className="w-full flex flex-col items-center justify-center m-auto">
-            <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-                
-
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
-                    
-                    <Link href="https://github.com/subhanahujha007" className="flex flex-row  items-center my-4 cursor-pointer hover:cursor-pointer">  
-                        <RxGithubLogo/>
-                        <span className="text-base ml-2">Github</span>
-                            </Link>
-
-                            <a href="https://github.com/subhanahujha007" className="flex items-center my-4 cursor-pointer">
-              <RxGithubLogo />
-              <span className="text-base ml-2">Github</span>
-            </a>
-            <a href="https://github.com/subhanahujha007" className="flex items-center my-4 cursor-pointer">
-              <RxGithubLogo />
-              <span className="text-base ml-2">Github</span>
-            </a>
-                    
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Social Media</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Instagram</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                     
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                  
-                        <span className="text-[15px] ml-[6px]">subhanshujha001@gmail.com</span>    
-                    </p>
-                </div>
-            </div>
-
-            <div className="mb-[20px] text-[15px] text-center">
-                &copy; Subhanshu Jha Dev 2024 Inc. All rights reserved
-            </div>
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] z-[999] pb-20 sm:pb-0">
+      <div className="w-full flex flex-col items-center justify-center m-auto">
+        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px]">Social Media</div>
+            {FooterSocialLinks?.map((social, index) => (
+              <Link href={social.href} key={index}>
+                <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                  {social.icon}
+                  <span className="text-[15px] ml-[6px]">{social.name}</span>
+                </p>
+              </Link>
+            ))}
+          </div>
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px]">About</div>
+            {FooterAboutLinks?.map((social, index) => (
+              <Link href={social.href} key={index}>
+                <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                  <span className="text-[15px] ml-[6px]">{social.name}</span>
+                </p>
+              </Link>
+            ))}
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="mb-[20px] text-[15px] text-center">
+          &copy; Subhanshu Jha 2024. All rights reserved
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
