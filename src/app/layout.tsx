a73be4f6598d162740179fc5a7c883e73add21d8
Subhanshu jha <138554397+subhanahujha007@users.noreply.github.com>
2026-05-20 11:20:51 +0530

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./Footer/page";
 import "./globals.css";
 import Navbar from "./Navbar/page";
 import StarsCanvas from "../app/Hero/content/starts/page";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,13 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow--x-hidden  `}>
+      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas/>
         <Navbar/>
         {children}
-              <script src="http://localhost:4000/sdk/widget.js" data-key="chatsop_ca830444600c9f31a7b58204d76dc21eed45de1f59b05a62" data-api-base-url="http://localhost:4000"></script>
-
-        </body>
+        <Script
+          src="http://localhost:4000/sdk/widget.js"
+          data-key="chatsop_ca830444600c9f31a7b58204d76dc21eed45de1f59b05a62"
+          data-api-base-url="http://localhost:4000"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
